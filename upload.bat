3ddeee12d4dee613458a4ccf9843dbb34eb94c5e
iTitus <iTitus@users.noreply.github.com>
2022-07-31 22:13:16 +0200

    Release

--- upload.bat
+++ upload.bat
@@ -1,9 +1,11 @@
 @echo off
 
-mvn package
+echo Building...
+call mvn package
 IF %ERRORLEVEL% NEQ 0 (
-  echo "Build failed..."
+  echo Build failed...
   exit %ERRORLEVEL%
 )
 
+echo Uploading...
 java -jar "C:/Program Files (x86)/Steam/steamapps/common/SlayTheSpire/mod-uploader.jar" upload -w steam_workshop
