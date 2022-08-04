d836a15a1a6b25924a39740c94f7693870f502ff
iTitus <iTitus@users.noreply.github.com>
2022-08-04 13:47:28 +0200

    Fix typo

--- src/main/java/io/github/ititus/downfallRelicStats/DownfallRelicStats.java
+++ src/main/java/io/github/ititus/downfallRelicStats/DownfallRelicStats.java
@@ -68,7 +68,7 @@ public final class DownfallRelicStats implements EditStringsSubscriber, PostInit
         register(DefensiveTrainingManualInfo.getInstance()); // Defensive Thesis
         register(DeflectingBracersInfo.getInstance()); // Deflecting Bracers
         register(DentedPlateInfo.getInstance()); // Dented Plate
-        register(DuelingGloveInfo.getInstance()); // Dueling GLove
+        register(DuelingGloveInfo.getInstance()); // Dueling Glove
         register(ExtraCursedKeyInfo.getInstance()); // Extra Cursed Key
         register(GladiatorsBookOfMartialProwessInfo.getInstance()); // Gladiators Manual
         register(GremlinBombInfo.getInstance()); // Gremlin Bomb
