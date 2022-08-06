3dab9834f28dba5ff76c24c416a354ccf02fadcb
iTitus <iTitus@users.noreply.github.com>
2022-08-07 01:05:04 +0200

    Add Ring of the Snek

--- src/main/java/io/github/ititus/downfallRelicStats/DownfallRelicStats.java
+++ src/main/java/io/github/ititus/downfallRelicStats/DownfallRelicStats.java
@@ -89,6 +89,7 @@ public final class DownfallRelicStats implements EditStringsSubscriber, PostInit
         register(ProtectiveGogglesInfo.getInstance()); // Protective Goggles
         register(CandleOfCauterizingInfo.getInstance()); // Red Candle
         register(RedScarfInfo.getInstance()); // Red Scarf
+        register(ConfusingCodexInfo.getInstance()); // Ring of the Snek
         register(RyeStalkInfo.getInstance()); // Rye Stalk
         register(SixitudeInfo.getInstance()); // Six-Point Brand
         register(SneckoSoulInfo.getInstance()); // Snecko Soul
