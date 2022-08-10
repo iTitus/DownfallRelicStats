43aec73c7a484c73d40b9bf43def60b1e061b7ca
iTitus <iTitus@users.noreply.github.com>
2022-08-10 13:56:08 +0200

    Reformat

--- src/main/java/io/github/ititus/downfallRelicStats/patches/relics/BrassTacksInfo.java
+++ src/main/java/io/github/ititus/downfallRelicStats/patches/relics/BrassTacksInfo.java
@@ -1,12 +1,13 @@
 package io.github.ititus.downfallRelicStats.patches.relics;
 
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
-import com.megacrit.cardcrawl.powers.MetallicizePower;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.powers.MetallicizePower;
 import hermit.relics.BrassTacks;
 import io.github.ititus.downfallRelicStats.BaseCombatRelicStats;
 
 public final class BrassTacksInfo extends BaseCombatRelicStats {
+
     private static final BrassTacksInfo INSTANCE = new BrassTacksInfo();
     private static final int BRASS_TACKS_DEFAULT_BLOCK_AMOUNT = 2;
 
@@ -20,14 +21,16 @@ public final class BrassTacksInfo extends BaseCombatRelicStats {
     }
 
     @SpirePatch(
-        clz = MetallicizePower.class,
-        method = "atEndOfTurnPreEndTurnCards"
+            clz = MetallicizePower.class,
+            method = "atEndOfTurnPreEndTurnCards"
     )
     @SuppressWarnings("unused")
     public static class Patch {
+
         public static void Postfix() {
-            if (AbstractDungeon.player.hasRelic(BrassTacks.ID))
+            if (AbstractDungeon.player.hasRelic(BrassTacks.ID)) {
                 getInstance().increaseAmount(BRASS_TACKS_DEFAULT_BLOCK_AMOUNT);
+            }
         }
     }
 }
