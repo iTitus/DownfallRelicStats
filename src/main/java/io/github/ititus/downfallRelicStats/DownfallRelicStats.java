967cd7e1b4f88632b0bf13b1adc561baa1885a56
iTitus <iTitus@users.noreply.github.com>
2022-08-11 15:28:08 +0200

    Add Unidentified Egg

--- src/main/java/io/github/ititus/downfallRelicStats/DownfallRelicStats.java
+++ src/main/java/io/github/ititus/downfallRelicStats/DownfallRelicStats.java
@@ -113,6 +113,7 @@ public final class DownfallRelicStats implements EditStringsSubscriber, PostInit
         register(TheBrokenSealInfo.getInstance()); // The Broken Seal
         register(SoulConsumerInfo.getInstance()); // Thermal Stone
         register(BolsterEngineInfo.getInstance()); // Tricky's Bolster-Rod
+        register(UnknownEggInfo.getInstance()); // Unidentified Egg
         register(WizardHatInfo.getInstance()); // Wizard Hat
         register(WoundPokerInfo.getInstance()); // Wound Poker
         register(BabySneckoInfo.getInstance()); // Young Snecko
