2bfaa0d9b3448e1ec5a6fcb2f9968fbeff86212c
iTitus <iTitus@users.noreply.github.com>
2025-12-21 12:55:57 +0100

    fix pickaxe

--- src/main/java/io/github/ititus/downfallRelicStats/relics/guardian/PickAxeInfo.java
+++ src/main/java/io/github/ititus/downfallRelicStats/relics/guardian/PickAxeInfo.java
@@ -1,74 +1,46 @@
 package io.github.ititus.downfallRelicStats.relics.guardian;
 
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
-import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.rooms.RestRoom;
-import com.megacrit.cardcrawl.screens.CardRewardScreen;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatches;
 import guardian.relics.PickAxe;
-import guardian.rewards.GemReward;
-import io.github.ititus.downfallRelicStats.BaseMultiCardRelicStats;
-import io.github.ititus.downfallRelicStats.BaseRelicStats;
-import io.github.ititus.downfallRelicStats.patches.track.SourceModifier;
+import io.github.ititus.downfallRelicStats.BaseCombatRelicStats;
+import io.github.ititus.downfallRelicStats.patches.editor.BeforeAfterMethodCallEditor;
+import javassist.expr.ExprEditor;
 
-import java.text.DecimalFormat;
+import java.util.ArrayList;
 
-public final class PickAxeInfo extends BaseRelicStats<PickAxeInfo.Stats> {
+public final class PickAxeInfo extends BaseCombatRelicStats {
 
     private static final PickAxeInfo INSTANCE = new PickAxeInfo();
 
     private PickAxeInfo() {
-        super(PickAxe.ID, Stats.class);
+        super(PickAxe.ID);
+        this.showPerTurn = false;
     }
 
     public static PickAxeInfo getInstance() {
         return INSTANCE;
     }
 
-    public static class Stats extends BaseMultiCardRelicStats.Stats {
-
-        int restSites = 0;
-
-        @Override
-        public String getDescription(String[] description) {
-            DecimalFormat df = new DecimalFormat("#.###");
-            int gemsMined = getCards().size();
-            return description[1] + gemsMined +
-                    description[2] + df.format((double) gemsMined / Math.max(1, restSites)) +
-                    (gemsMined > 0 ? super.getDescription(description) : "");
-        }
-    }
-
-    @SpirePatch(
-            clz = CardRewardScreen.class,
-            method = "acquireCard"
-    )
+    @SpirePatches({
+            @SpirePatch(
+                    clz = PickAxe.class,
+                    method = "onEquip"
+            ),
+            @SpirePatch(
+                    clz = PickAxe.class,
+                    method = "onVictory"
+            )
+    })
     @SuppressWarnings("unused")
     public static class Patch1 {
 
-        public static void Postfix(CardRewardScreen __instance, AbstractCard card) {
-            if (AbstractDungeon.isPlayerInDungeon() && AbstractDungeon.player.hasRelic(PickAxe.ID)) {
-                if (AbstractDungeon.getCurrRoom() instanceof RestRoom) {
-                    SourceModifier mod = SourceModifier.get(card);
-                    if (mod != null && mod.getRewardItem() instanceof GemReward) {
-                        getInstance().stats.addCard(card);
-                    }
-                }
-            }
+        public static ExprEditor Instrument() {
+            return new BeforeAfterMethodCallEditor(ArrayList.class, "add", Patch1.class, false, true);
         }
-    }
 
-    @SpirePatch(
-            clz = RestRoom.class,
-            method = "onPlayerEntry"
-    )
-    @SuppressWarnings("unused")
-    public static class Patch2 {
-
-        public static void Postfix() {
-            if (AbstractDungeon.player.hasRelic(PickAxe.ID)) {
-                getInstance().stats.restSites++;
-            }
+        public static void after() {
+            getInstance().trigger();
         }
     }
 }
