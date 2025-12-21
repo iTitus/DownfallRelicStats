015aa9eb9300b27e1eb69402c12a2fd3a5f39631
iTitus <iTitus@users.noreply.github.com>
2025-12-21 13:17:30 +0100

    fix loaded die

--- src/main/java/io/github/ititus/downfallRelicStats/relics/snecko/LoadedDieInfo.java
+++ src/main/java/io/github/ititus/downfallRelicStats/relics/snecko/LoadedDieInfo.java
@@ -1,13 +1,10 @@
 package io.github.ititus.downfallRelicStats.relics.snecko;
 
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
-import com.evacipated.cardcrawl.modthespire.lib.SpirePatches;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import io.github.ititus.downfallRelicStats.BaseCombatRelicStats;
 import io.github.ititus.downfallRelicStats.patches.editor.ConstructorHookEditor;
 import javassist.expr.ExprEditor;
-import sneckomod.actions.MuddleAction;
-import sneckomod.actions.MuddleMarkedAction;
 import sneckomod.relics.LoadedDie;
 
 public final class LoadedDieInfo extends BaseCombatRelicStats {
@@ -22,16 +19,11 @@ public final class LoadedDieInfo extends BaseCombatRelicStats {
         return INSTANCE;
     }
 
-    @SpirePatches({
-            @SpirePatch(
-                    clz = MuddleAction.class,
-                    method = "update"
-            ),
-            @SpirePatch(
-                    clz = MuddleMarkedAction.class,
-                    method = "update"
-            )
-    })
+
+    @SpirePatch(
+            clz = LoadedDie.class,
+            method = "onTrigger"
+    )
     @SuppressWarnings("unused")
     public static class Patch {
 
