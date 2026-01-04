f26fd5224ce25f45a5d26edd4d72bfd36d6d3614
iTitus <iTitus@users.noreply.github.com>
2026-01-04 18:11:31 +0100

    fix Defensive Training Manual

--- src/main/java/io/github/ititus/downfallRelicStats/relics/champ/DefensiveTrainingManualInfo.java
+++ src/main/java/io/github/ititus/downfallRelicStats/relics/champ/DefensiveTrainingManualInfo.java
@@ -11,7 +11,6 @@ import javassist.expr.ExprEditor;
 public final class DefensiveTrainingManualInfo extends BaseCombatRelicStats {
 
     private static final DefensiveTrainingManualInfo INSTANCE = new DefensiveTrainingManualInfo();
-    private static final int DEFENSIVE_FINISHER_DEFAULT_BLOCK_AMOUNT = 8;
 
     private DefensiveTrainingManualInfo() {
         super(DefensiveTrainingManual.ID);
@@ -33,7 +32,7 @@ public final class DefensiveTrainingManualInfo extends BaseCombatRelicStats {
         }
 
         public static void hook(int blockAmount) {
-            getInstance().increaseAmount(blockAmount - DEFENSIVE_FINISHER_DEFAULT_BLOCK_AMOUNT);
+            getInstance().increaseAmount(DefensiveTrainingManual.OOMPH);
         }
     }
 }
